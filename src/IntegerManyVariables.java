e0e5bb2b5dd718ca84d4ff63b2ea35d9040bd802
SQY USER <jwala.maurya@squareyards.co.in>
2024-11-27 11:21:46 +0530

    Second commit

---
+++ src/IntegerManyVariables.java
@@ -0,0 +1,7 @@
+// create java many varibale integer type 
+public class IntegerManyVariables {
+    public static void main(String[] args){
+int x = 10 , y =20, z = 30;
+System.out.println(x + y + z);
+    }
+}
